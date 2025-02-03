1cc680dedf9d576316606b7f4e88d585af3d32cd
Louka <louka.pesic@gmail.com>
2025-02-03 11:48:11 +0100

    Changements dans le solveur

--- src/src/Solveur.java
+++ src/src/Solveur.java
@@ -1,5 +1,34 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solveur {
-	
+
+	private Grille grille;
+	private List<String> log;
+	private List<Grille> hypotheses;
+
+
+	public Solveur(Grille grille) {
+		this.grille = grille;
+	}
+
+
+	void retourTrace() {
+		hypotheses.add(grille);
+		for (int i = 0; i < grille.GetTaille() * grille.GetTaille(); i++) {
+			if (grille.getValeur(i).equals(0)) {
+				for (int j = 0; j < grille.GetTaille(); j++) {
+					grille.setValeur(i, grille.elements[j]);
+					//insérer ici une boucle for skippant l'action à chaque fois que le chiffre testé est présent dans la ligne/colonne/SousGrille
+				}
+				hypotheses.add(grille);
+			}
+		} 
+
+		
+		return;
+	}
+
 }
