1c55ab7e7a10ec8812551b3a38c76bd7acc57a67
juliett52 <juliettrojas01@gmail.com>
2025-02-08 22:16:05 +0100

    Modifié hauteur + largeur pour dévenir juste taille. Rajout que l'utilisateur peut remplir un sudoku qu'il souhaite résoudre. Amélioration des codes pour vérifier que le sudoku généré est résolvable.

--- src/src/Grille.java
+++ src/src/Grille.java
@@ -1,35 +1,34 @@
 package src;
 
-import java.lang.Cloneable;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Grille {
-    private int hauteur;
-    private int largeur;
-    private int difficulte;
+    private static final char[] LETTRES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private int taille;
+    private float difficulte;
     private int type;
     private int symbole;
     int[][] grid;
+    char[][] letterGrid;
 
-    public Grille(int hauteur, int largeur, int difficulte, int type, int symbole) {
-        this.hauteur = hauteur;
-        this.largeur = largeur;
+    public Grille(int taille, int difficulte, int type, int symbole) {
+        this.taille = taille;
         this.difficulte = difficulte;
         this.type = type;
         this.symbole = symbole;
     }
 
     public int[][] genererGrille() {
-        int gridSize = hauteur * largeur; // Total grid size (e.g., 9x9 for 3x3 blocks)
-        int[][] grid = new int[gridSize][gridSize];
+        int[][] grid = new int[taille][taille];
         fillDiagonalBoxes(grid);
-        solveSudoku(grid);
+        solveSudoku(grid, 0, 0);
         removeNumbers(grid);
         return grid;
     }
 
     private void fillDiagonalBoxes(int[][] grid) {
-        int boxSize = hauteur; // Hauteur of a single block
+        int boxSize = (int) Math.sqrt(taille);
         for (int i = 0; i < grid.length; i += boxSize) {
             fillBox(grid, i, i);
         }
@@ -37,35 +36,153 @@ public class Grille {
 
     private void fillBox(int[][] grid, int startRow, int startCol) {
         Random rand = new Random();
-        boolean[] used = new boolean[grid.length + 1];
+        int boxSize = (int) Math.sqrt(taille);
+        int[] numbers = new int[taille];
 
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < largeur; j++) {
-                int num;
-                do {
-                    num = rand.nextInt(grid.length) + 1;
-                } while (used[num]);
-                used[num] = true;
-                grid[startRow + i][startCol + j] = num;
+        // générer une liste de nombres uniques
+        for (int i = 0; i < taille; i++) {
+            numbers[i] = i + 1;
+        }
+
+        // mélanger les nombres
+        for (int i = 0; i < taille; i++) {
+            int j = rand.nextInt(taille);
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+
+        // remplir la box avec les nombres uniques
+        int index = 0;
+        for (int i = 0; i < boxSize; i++) {
+            for (int j = 0; j < boxSize; j++) {
+                grid[startRow + i][startCol + j] = numbers[index++];
             }
         }
     }
 
-    private boolean solveSudoku(int[][] grid) {
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[row].length; col++) {
-                if (grid[row][col] == 0) {
-                    for (int num = 1; num <= grid.length; num++) {
-                        if (isSafe(grid, row, col, num)) {
-                            grid[row][col] = num;
+    public boolean fournirGrille(Scanner scanner) {
+        System.out.print("Voulez-vous entrer votre propre Sudoku ? (true/false): ");
+        return Boolean.parseBoolean(scanner.nextLine());
+    }
 
-                            if (solveSudoku(grid)) {
-                                return true;
-                            }
+    private boolean solveSudoku(int[][] grid, int row, int col) {
+        if (row == taille) {
+            return true; // sudoku résolu
+        }
+        if (col == taille) {
+            return solveSudoku(grid, row + 1, 0); // passe à la ligne suivante
+        }
+        if (grid[row][col] != 0) {
+            return solveSudoku(grid, row, col + 1); // continue si la case est déjà remplie
+        }
 
-                            grid[row][col] = 0;
-                        }
-                    }
+        for (int num = 1; num <= taille; num++) {
+            if (isSafe(grid, row, col, num)) { // vérifie si le placement est valide
+                grid[row][col] = num;
+
+                if (solveSudoku(grid, row, col + 1)) {
+                    return true; // retourne vrai si une solution est trouvée
+                }
+
+                grid[row][col] = 0; // retour en arrière
+            }
+        }
+        return false; // Pas de solution trouvée
+    }
+
+    private void removeNumbers(int[][] grid) {
+        Random rand = new Random();
+        int cellsToRemove = (int) (grid.length * grid[0].length * difficulte) / 7;
+
+        while (cellsToRemove > 0) {
+            int row = rand.nextInt(grid.length);
+            int col = rand.nextInt(grid[0].length);
+            if (grid[row][col] != 0) {
+                int backup = grid[row][col]; // sauvegarde le nombre
+                grid[row][col] = 0; // enleve le nombre
+
+                if (!hasUniqueSolution(grid)) { // si grille a plusieurs solutions alors on remet le nombre
+                    grid[row][col] = backup;
+                } else {
+                    cellsToRemove--; // prend en compte que les effacements valides
+
+                }
+            }
+        }
+    }
+
+    private boolean hasUniqueSolution(int[][] grid) {
+        int[][] tempGrid = copyGrid(grid); // copie de la grille pour ne pas modifier l'originale
+        return countSolutions(tempGrid, 0, 0) == 1; // verfiex si une seule solution existe
+    }
+
+    private int countSolutions(int[][] grid, int row, int col) {
+        if (row == taille) { // si on est à la fin de la grille
+            return 1;
+        }
+
+        if (col == taille) { // avancer à la ligne suivante
+            return countSolutions(grid, row + 1, 0);
+        }
+
+        if (grid[row][col] != 0) { // sauter si la case est déjà remplie
+            return countSolutions(grid, row, col + 1);
+        }
+
+        int count = 0;
+        for (int num = 1; num <= taille; num++) {
+            if (isSafe(grid, row, col, num)) {
+                grid[row][col] = num;
+                count += countSolutions(grid, row, col + 1);
+                if (count > 1) { // arreter si plus d'une solution est trouvée
+                    return count;
+                }
+                grid[row][col] = 0; // retour en arrière
+            }
+        }
+
+        return count;
+    }
+
+    private int[][] copyGrid(int[][] grid) {
+        int[][] newGrid = new int[taille][taille];
+        for (int i = 0; i < taille; i++) {
+            newGrid[i] = grid[i].clone();
+        }
+        return newGrid;
+    }
+
+    private boolean isSafe(int[][] grid, int row, int col, int num) {
+        int boxSize = (int) Math.sqrt(taille);
+        return isRowValid(grid, row, num) &&
+                isColValid(grid, col, num) &&
+                isBoxValid(grid, row - row % boxSize, col - col % boxSize, num);
+    }
+
+    private boolean isRowValid(int[][] grid, int row, int num) {
+        for (int col = 0; col < taille; col++) {
+            if (grid[row][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isColValid(int[][] grid, int col, int num) {
+        for (int row = 0; row < taille; row++) {
+            if (grid[row][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isBoxValid(int[][] grid, int boxStartRow, int boxStartCol, int num) {
+        int boxSize = (int) Math.sqrt(taille);
+        for (int row = 0; row < boxSize; row++) {
+            for (int col = 0; col < boxSize; col++) {
+                if (grid[boxStartRow + row][boxStartCol + col] == num) {
                     return false;
                 }
             }
@@ -73,77 +190,86 @@ public class Grille {
         return true;
     }
 
-    private void removeNumbers(int[][] grid) {
-        Random rand = new Random();
-        // change this into a percentage so the difficulty is more adjustable and works
-        // better
-        if (difficulte == 1) {
-            difficulte = 10;
-        }
+    public void afficherGrilleInt(int[][] grid) {
+        int boxSize = (int) Math.sqrt(taille);
 
-        int cellsToRemove = (int) (grid.length * grid[0].length * difficulte) / 7; // Adjust the number of cells to
-                                                                                   // remove based on difficulty
-
-        while (cellsToRemove > 0) {
-            int row = rand.nextInt(grid.length);
-            int col = rand.nextInt(grid[0].length);
-
-            if (grid[row][col] != 0) {
-                grid[row][col] = 0;
-                cellsToRemove--;
-            }
-        }
-    }
-
-    public boolean isSafe(int[][] grid, int row, int col, int num) {
-        return !isInRow(grid, row, num) && !isInCol(grid, col, num) && !isInBox(grid, row, col, num);
-    }
-
-    private boolean isInRow(int[][] grid, int row, int num) {
-        for (int col = 0; col < grid[row].length; col++) {
-            if (grid[row][col] == num) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isInCol(int[][] grid, int col, int num) {
         for (int row = 0; row < grid.length; row++) {
-            if (grid[row][col] == num) {
-                return true;
+            if (row % boxSize == 0 && row != 0) {
+                System.out.println("-".repeat(grid.length * 2 + boxSize - 1));
             }
+            for (int col = 0; col < grid[row].length; col++) {
+                if (col % boxSize == 0 && col != 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(grid[row][col] == 0 ? ". " : grid[row][col] + " ");
+            }
+            System.out.println();
         }
-        return false;
+        System.out.print("Est-ce que voulez rejouer? (true/false): ");
+        boolean answer = Boolean.parseBoolean(new Scanner(System.in).nextLine());
+        if (answer) {
+            Main.main(new String[0]);
+        }
     }
 
-    private boolean isInBox(int[][] grid, int row, int col, int num) {
-        int startRow = row - row % hauteur;
-        int startCol = col - col % largeur;
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < largeur; j++) {
-                if (grid[startRow + i][startCol + j] == num) {
-                    return true;
+    public void fillGridWithLetters() {
+        letterGrid = new char[taille][taille];
+        int letterIndex = 0;
+        for (int row = 0; row < letterGrid.length; row++) {
+            for (int col = 0; col < letterGrid[row].length; col++) {
+                letterGrid[row][col] = LETTRES[letterIndex % LETTRES.length];
+                letterIndex++;
+            }
+        }
+        afficherGrilleChar(letterGrid);
+    }
+
+    public int[][] fillGridWithNumbers(Scanner scanner) {
+        grid = new int[taille][taille];
+        for (int row = 0; row < letterGrid.length; row++) {
+            for (int col = 0; col < letterGrid[row].length; col++) {
+                while (true) {
+                    System.out.print("Entrez un nombre pour (" + row + ", " + col + ") [" + letterGrid[row][col] + "]: ");
+                    try {
+                        int number = Integer.parseInt(scanner.nextLine());
+                        if (number >= 0 && number <= taille) {
+                            grid[row][col] = number;
+                            break;
+                        } else {
+                            System.out.println("Erreur: Entrez un nombre entre 0 et " + taille);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Entrée invalide. Veuillez entrer un nombre.");
+                    }
                 }
             }
         }
-        return false;
+        return grid;
+    }
+
+    public void afficherGrilleChar(char[][] grid) {
+        for (char[] row : grid) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("Est-ce que voulez rejouer? (true/false): ");
+        boolean answer = Boolean.parseBoolean(new Scanner(System.in).nextLine());
+        if (answer) {
+            Main.main(new String[0]);
+        }
     }
 
     public int getValeur(int i, int j) {
         return grid[i][j];
     }
 
-    public int GetH() {
-        return hauteur;
-    }
-
-    public int GetL() {
-        return largeur;
+    public int GetTaille() {
+        return taille;
     }
 
     public void setValeur(int i, int j, int v) {
         grid[i][j] = v;
     }
-
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -4,78 +4,94 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Bienvenue au jeu Sudoku!");
 
-        // Get grid dimensions and difficulty level from the user
-        System.out.println("Welcome to Custom Sudoku Generator!");
-        int hauteur = 0;
-        int largeur = 0;
+        int taille = 0;
         int difficulte = 0;
+        int type = 0;
+        int symbole = 0;
 
-        System.out.print("Enter the block hauteur: ");
-        try {
-            hauteur = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input for hauteur. Please enter a number.");
-            return;
-        }
-
-        System.out.print("Enter the block largeur: ");
-        try {
-            largeur = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input for largeur. Please enter a number.");
-            return;
-        }
-
-        System.out.print("Enter the difficulty level (1-5): ");
-        try {
-            difficulte = Integer.parseInt(scanner.nextLine());
-            if (difficulte < 1 || difficulte > 5) {
-                System.out.println("Invalid input for difficulty. Please enter a number between 1 and 5.");
-                return;
+        while (true) {
+            System.out.print("Entrez la taille du Sudoku (4, 9, 16...): ");
+            try {
+                taille = Integer.parseInt(scanner.nextLine());
+                if (!isValidSudokuSize(taille)) {
+                    System.out.println("Erreur: La taille doit être un carré parfait (4, 9, 16...).");
+                    continue;
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Entrée invalide. Veuillez entrer un nombre.");
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input for difficulty. Please enter a number.");
-            return;
         }
 
-        Grille g = new Grille(hauteur, largeur, difficulte, 0, 0);
+        // création temporaire de l'objet pour vérifier si l'utilisateur veut entrer sa grille
+        Grille g = new Grille(taille, difficulte, type, symbole);
 
-        // Ensure grid size is at least 4
-        if (hauteur * largeur < 4) {
-            System.out.println("Invalid grid size! Please ensure hauteur * largeur is at least 4.");
-            return;
+        boolean fournirGrille = g.fournirGrille(scanner);
+
+        if (fournirGrille) {
+            System.out.println("Veuillez remplir votre propre grille :");
+            g.fillGridWithLetters();
+            int[][] userSudoku = g.fillGridWithNumbers(scanner);
+            System.out.println("Votre grille entrée :");
+            g.afficherGrilleInt(userSudoku);
+        } else {
+            while (true) {
+                System.out.print("Entrez le niveau de difficulté (1-5): ");
+                try {
+                    difficulte = Integer.parseInt(scanner.nextLine());
+                    if (difficulte < 1 || difficulte > 5) {
+                        System.out.println("Erreur: Veuillez entrer un nombre entre 1 et 5.");
+                    } else {
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrée invalide. Veuillez entrer un nombre.");
+                }
+            }
+
+            while (true) {
+                System.out.print("Entrez le type de Sudoku (0: classique, 1: en X, 2: en +): ");
+                try {
+                    type = Integer.parseInt(scanner.nextLine());
+                    if (type < 0 || type > 2) {
+                        System.out.println("Erreur: Veuillez entrer 0, 1 ou 2.");
+                    } else {
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrée invalide. Veuillez entrer un nombre.");
+                }
+            }
+
+            while (true) {
+                System.out.print("Entrez le symbole du Sudoku (0: numéros, 1: lettres, 2: emojis): ");
+                try {
+                    symbole = Integer.parseInt(scanner.nextLine());
+                    if (symbole < 0 || symbole > 2) {
+                        System.out.println("Erreur: Veuillez entrer 0, 1 ou 2.");
+                    } else {
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrée invalide. Veuillez entrer un nombre.");
+                }
+            }
+
+            // création de l'objet après avoir collecté toutes les infos
+            g = new Grille(taille, difficulte, type, symbole);
+
+            int[][] sudoku = g.genererGrille();
+            System.out.println("Sudoku généré :");
+            g.afficherGrilleInt(sudoku);
         }
-
-        // Generate and display one Sudoku puzzle
-        int[][] puzzle = g.genererGrille();
-        System.out.println("Generated Sudoku Puzzle:");
-        printPuzzle(puzzle);
     }
 
-    // Utility method to print the Sudoku puzzle
-    private static void printPuzzle(int[][] grid) {
-        int hauteur = (int) Math.sqrt(grid.length);
-        int largeur = hauteur;
-
-        for (int row = 0; row < grid.length; row++) {
-            if (row % hauteur == 0 && row != 0) {
-                System.out.println("-".repeat(grid.length * 2 + hauteur - 1));
-            }
-            for (int col = 0; col < grid[row].length; col++) {
-                if (col % largeur == 0 && col != 0) {
-                    System.out.print("| ");
-                }
-                System.out.print(grid[row][col] == 0 ? ". " : grid[row][col] + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.print("Do you want to generate another puzzle? (true/false): ");
-        boolean answer = Boolean.parseBoolean(scanner.nextLine());
-        if (answer) {
-            main(new String[0]);
-        }
+    private static boolean isValidSudokuSize(int size) {
+        int sqrt = (int) Math.sqrt(size);
+        return sqrt * sqrt == size;
     }
 }
